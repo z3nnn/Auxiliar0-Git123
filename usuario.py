5534a277b0d2b089d735eeee308b6355ba517a17
Franzkier <140219655+fgonzalezurriola@users.noreply.github.com>
2024-08-14 19:01:00 -0400

    FEAT changed usuario

--- usuario.py
+++ usuario.py
@@ -7,3 +7,9 @@ class Usuario:
 
     def agregarTarea(self, tarea):
         self.tareas.append(tarea)
+    def listarTareas(self):
+        for tarea in self.tareas:
+            if tarea.estaLista():
+                print(f"[X] {tarea.obtenerNombre()}" )
+            else:
+                print(f"[ ] {tarea.obtenerNombre()}" )
